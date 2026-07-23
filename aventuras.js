9c8f34dc28fd7c737a8f7484e21a541a50200327
Andrexbt <andre.batista.aab@gmail.com>
2026-07-23 14:18:59 -0300

    Início da contrução da tela de aventuras

---
+++ aventuras.js
@@ -0,0 +1,20 @@
+"use strict";
+
+const cenaInicial = {
+
+  titulo: "A Fuga",
+
+  contexto:
+    "Por algum motivo, você está fugindo da cidade onde viveu pelos últimos 4 anos.",
+
+  objetivo:
+    "Escapar da cidade."
+};
+
+console.log(cenaInicial);
+
+console.log(cenaInicial.titulo);
+
+console.log(cenaInicial.contexto);
+
+console.log(cenaInicial.objetivo);
